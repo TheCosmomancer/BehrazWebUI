ec9201a25d3c07061d528b6bf72ae741ab67fddd
Farbod Khalili Khoshehmehr <lecosmomancer@gmail.com>
2025-09-18 18:14:56 +0330

    Changed layer to top (same as waybar), made the size dynamic and changed the toggle to use opacity instead of panelsize to prevent tearing while transitioning

--- ai_panel.py
+++ ai_panel.py
@@ -9,8 +9,8 @@ screen = Gdk.Screen.get_default()
 screen_width = screen.get_width()
 screen_height = screen.get_height()
 # --- Configurable Panel Settings ---
-PANEL_WIDTH = 1000   # easily change width
-PANEL_HEIGHT = screen_height -50  # easily change height
+PANEL_WIDTH = (screen_width * 2) // 5   # easily change width
+PANEL_HEIGHT = screen_height - 50  # easily change height
 ANIMATION_STEPS = 20  # more steps = smoother animation
 ANIMATION_DELAY = 15  # ms per step (~60fps)
 
@@ -21,7 +21,7 @@ class AIPanel(Gtk.Window):
         GtkLayerShell.init_for_window(self)
 
         # Overlay layer
-        GtkLayerShell.set_layer(self, GtkLayerShell.Layer.OVERLAY)
+        GtkLayerShell.set_layer(self, GtkLayerShell.Layer.TOP)
         GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.LEFT, True)
         GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.TOP, True)
         GtkLayerShell.set_anchor(self, GtkLayerShell.Edge.BOTTOM, False)
@@ -41,7 +41,8 @@ class AIPanel(Gtk.Window):
         self.add(self.webview)
 
         self.visible = False
-        self.set_default_size(0, PANEL_HEIGHT)  # start hidden
+        self.set_opacity(0.0)
+        self.set_default_size(PANEL_WIDTH, PANEL_HEIGHT)  # start hidden
         self.hide()
 
     def slide_in(self):
@@ -49,30 +50,30 @@ class AIPanel(Gtk.Window):
         self.present()
         self.grab_focus()
         self.visible = True
-        step = PANEL_WIDTH // ANIMATION_STEPS
-        self.current_width = 0
-
+        step = 1 / ANIMATION_STEPS
+        self.current_opacity = 0
+        
         def grow():
-            if self.current_width < PANEL_WIDTH:
-                self.current_width += step
-                if self.current_width > PANEL_WIDTH:
-                    self.current_width = PANEL_WIDTH
-                self.set_size_request(self.current_width, PANEL_HEIGHT)
+            if self.current_opacity < 1:
+                self.current_opacity += step
+                if self.current_opacity > 1:
+                    self.current_opacity = 1
+                self.set_opacity(self.current_opacity)
                 return True
             return False
 
         GLib.timeout_add(ANIMATION_DELAY, grow)
 
     def slide_out(self):
-        step = PANEL_WIDTH // ANIMATION_STEPS
-        self.current_width = PANEL_WIDTH
+        step = 1 / ANIMATION_STEPS
+        self.current_opacity = 1
 
         def shrink():
-            if self.current_width > 0:
-                self.current_width -= step
-                if self.current_width < 0:
-                    self.current_width = 0
-                self.set_size_request(self.current_width, PANEL_HEIGHT)
+            if self.current_opacity > 0:
+                self.current_opacity -= step
+                if self.current_opacity < 0:
+                    self.current_opacity = 0
+                self.set_opacity(self.current_opacity)
                 return True
             else:
                 self.hide()
